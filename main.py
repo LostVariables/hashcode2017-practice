ee3ceee27cdf60b722daa65de6a4704f358e96f5
Aristotelis Dossas <teldosas@gmail.com>
2017-01-30 20:32:31 +0000

    Add function that counts tomatoes and mushrooms of a slice

---
+++ main.py
@@ -0,0 +1,20 @@
+
+def solve(filename):
+    f = open("inputs/"+filename+".in")
+
+    R, C, L, H = [int(x) for x in f.readline().split()]
+
+    pizza = []
+    for i in range(R):
+        pizza.append(f.readline())
+
+    def count(r1, c1, r2, c2):
+        rows = [pizza[i][c1:c2+1] for i in range(r1, r2 + 1)]
+
+        reduceFunc = lambda (t,m), c: (t+1,m) if c == 'T' else (t,m+1)
+        perRow = [reduce(reduceFunc, row, (0,0)) for row in rows]
+
+        return reduce(lambda (tt,tm),(t,m): (tt+t, tm+m), perRow, (0,0))
+
+
+solve("small")
